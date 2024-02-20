4c9d0f451411faffd699eb32032f0db87337594b
jiylee <jjenny4711@gmail.com>
2024-02-20 00:13:41 -0500

    fixed 3

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let news=[]
 const getLatestNews=async()=>{
   try{
-    const url= new URL(`https://main--newsapi2024.netlify.app`)
+     const url= new URL(`https://newsapi2024.netlify.app`)
+   // const url= new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
     const res = await fetch(url)
     const data=await res.json()
      news = data.articles
