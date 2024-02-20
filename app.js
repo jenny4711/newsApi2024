a99df19ff8565656d27fad5a0718bce43c26e430
jiylee <jjenny4711@gmail.com>
2024-02-20 00:56:24 -0500

    fixed 12

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ let news=[]
 const getLatestNews=async()=>{
   try{
      const url= new URL(`https://newsapi2024.netlify.app`)
-   // const url= new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
+  
     const res = await fetch(url)
     const data=await res.json()
      news = data.articles
-    console.log(news,'res')
-    console.log(data,'data')
+    console.log(news)
+    console.log(data)
 
   }catch(error){
     console.log('error-getLatestNews',error)
