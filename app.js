4594d0d36367140a8d328a8f3643089f16b4cd1b
jiylee <jjenny4711@gmail.com>
2024-02-21 00:59:12 -0500

    9-10

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 let newsList=[]
+//const BASE_URL=`https://newsapi2024.netlify.app/top-headlines?`
+const BASE_URL=`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?`
+const menus = document.querySelectorAll('.menus button')
+
+const searchBtn=document.getElementById('searchBtn')
+
+const getNewsByKeyword=async()=>{
+  try{
+   const keyword =document.getElementById('search-input').value
+   const url = new URL(`${BASE_URL}q=${keyword}`)
+   const res = await fetch(url)
+   const data=await res.json()
+ 
+  newsList = data.articles
+  render()
+  }catch(error){
+    console.log(error,'error')
+  }
+}
+
+menus.forEach((btn)=>{
+  btn.addEventListener('click',(evt)=>getNewsByCategory(evt))
+})
+
+const getNewsByCategory=async(evt)=>{
+  try{
+  
+   const category=evt.target.textContent.toLowerCase();
+   
+  const url= new URL(`${BASE_URL}category=${category}`)
+  const res = await fetch(url)
+  const data=await res.json()
+
+  newsList = data.articles
+  render()
+  }catch(error){
+    console.log(error,'error!!!!!!')
+  }
+}
+
 const getLatestNews=async()=>{
   try{
-     const url= new URL(`https://newsapi2024.netlify.app/top-headlines?`)
+     const url= new URL(BASE_URL)
     // const url= new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?`)
   
     const res = await fetch(url)
