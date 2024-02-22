19d918b43b06f8b61d1db8da2052611f421c8999
jiylee <jjenny4711@gmail.com>
2024-02-22 02:09:42 -0500

    pagination

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ let newsList=[]
 const BASE_URL=`https://newsapi2024.netlify.app/top-headlines?`
 //const BASE_URL= `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?`
 let url=new URL(`${BASE_URL}`)
+let totalResult = 0
+let page=1
+let pageSize=10
+let groupSize=5
 const menus = document.querySelectorAll('.menus button')
 
 const searchBtn=document.getElementById('searchBtn')
 const getNews=async()=>{
   try{
-    
+    url.searchParams.set("page",page) //&page=page
+    url.searchParams.set("pageSize",pageSize)
     const res = await fetch(url)
+
     const data=await res.json()
+    console.log(data,'data')
    if(res.status !==200)throw new Error(data.message)
    if(data.articles.length === 0)throw new Error('NO Result for this search!')
   newsList = data.articles
+totalResult = data.totalResults
 
   render()
+  paginationRender()
   }catch(error){
     errorRender(error)
 console.log(error.message,'eeeeee')
@@ -97,5 +106,36 @@ newsHTML=newsList.map((news)=>`<div class="row news">
 
   document.getElementById('news-board').innerHTML=newsHTML
 }
+
+
+const paginationRender=()=>{
+ 
+let totalPages =Math.ceil(totalResult/pageSize)
+ let pageGroup=Math.ceil(page/groupSize);
+  let lastPage = pageGroup * groupSize;
+
+  if(lastPage >totalPages){
+    lastPage=totalPages
+  }
+ let firstPage = lastPage-(groupSize-1) <=0?1:lastPage-(groupSize-1)
+
+let paginationHTML= ``
+for(let i=firstPage;i<=lastPage;i++){
+  paginationHTML+=`    <li class="page-item ${i===page?"active":""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+}
+ document.querySelector('.pagination').innerHTML=paginationHTML
+
+
+
+}
+
+
+const moveToPage=(pageNum)=>{
+  page=pageNum;
+  getNews()
+}
+
+
+
 
 getLatestNews()
